da17607c1799ed3c6f4b9652a06971f9105ec2e2
ropey <nicolasborromei@gmail.com>
2020-02-11 18:22:37 -0300

    Revert "Update index.js"
    This reverts commit 6abe8fb4ef546730e3dd56e1c71a801aa132d803.

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -1,4 +1,8 @@
-import React, { useState, useEffect, useCallback } from 'react';
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react/no-this-in-sfc */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { RNCamera } from 'react-native-camera';
 import { View, Text, TouchableOpacity } from 'react-native';
@@ -8,36 +12,34 @@ import styles from './styles';
 
 const Camera = ({ title }) => {
   const [path, setPath] = useState('');
-  const [camera, setCamera] = useState();
 
-  useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log(path);
-  });
-
-  const cameraHook = useCallback(ref => {
-    setCamera(ref);
-  }, []);
-  const takePictureHook = useCallback(() => takePicture(this), []);
-
-  /* global takePicture:true */
   takePicture = async () => {
-    // eslint-disable-next-line no-console
     console.log('SNAP');
 
-    if (camera) {
+    if (this.camera) {
       const options = { quality: 0.5, base64: true };
-      const data = await camera.takePictureAsync(options);
+      const data = await this.camera.takePictureAsync(options);
 
+      console.log(data.uri);
       setPath(data.uri);
+      console.log(path);
     }
   };
 
   return (
     <View style={styles.container}>
-      <RNCamera ref={cameraHook} style={styles.camera} captureAudio={false} />
+      <RNCamera
+        ref={ref => {
+          this.camera = ref;
+        }}
+        style={styles.camera}
+        captureAudio={false}
+      />
       <Text style={styles.title}>{title}</Text>
-      <TouchableOpacity style={styles.shotTouchable} onPress={takePictureHook}>
+      <TouchableOpacity
+        style={styles.shotTouchable}
+        onPress={() => this.takePicture(this)}
+      >
         <IconButton icon='circle' color={Colors.white} size={70} />
       </TouchableOpacity>
     </View>
