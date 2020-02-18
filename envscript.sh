2e85e490d150a764dfdeb1c3e1027522d81cdc34
Alejandro <alejandroce@lagash.com>
2020-02-18 16:56:01 -0300

    ios build

--- envscript.sh
+++ envscript.sh
@@ -5,7 +5,6 @@ then
   echo "Switching to Production environment"
   yes | cp -rf "credentials/production/prod.keystore" android/app
 else
-else
   echo "Switching to Development environment"
   yes | cp -rf "credentials/development/dev.keystore" android/app
 fi
